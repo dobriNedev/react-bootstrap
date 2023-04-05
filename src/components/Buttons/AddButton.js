501ea940ba1fe6416c7dfa99017b3e296ef9ae40
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-05 13:58:12 +0200

    initial Button component

---
+++ src/components/Buttons/AddButton.js
@@ -0,0 +1,8 @@
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const MyButton = ({ text }) => {
+  return <Button>{text}</Button>;
+};
+
+export default MyButton;
