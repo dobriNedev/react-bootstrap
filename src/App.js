6ec768a59c14b63550e3635fa642cee13d57c554
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-06 10:58:14 +0200

    App with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyNavbar from "./components/Navbar/Navbar";
+import PostProvider from "./contexts/postsContext";
+import Routing from "./components/Routing/Routing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PostProvider>
+      <div className="App">
+        <MyNavbar />
+        <Routing />
+      </div>
+    </PostProvider>
   );
 }
 
